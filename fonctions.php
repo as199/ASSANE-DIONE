dfc46acea04957158d42fe514c4228335c9e8693
as199 <57167524+as199@users.noreply.github.com>
2020-04-05 02:29:23 +0000

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -1,5 +1,49 @@
 <?php
+  // fonction F3
+function CorrigePhrases($texte)
+{
+//Nous allons tenter de recuperer l'ensemble des phrases.
+if(preg_match_all("#[A-Za-z]{1}([^.!?]|([.][0-9]))*[.!?]#m", $texte,$phrases)){
+//Recuperation des phrases et l'insertion dans un tableau...
+  foreach ($phrases[0] as $value)
+  {
+    $value = preg_replace('#\s\s+#'," ", $value);// c'est une     espace
+    $value = preg_replace('#\’\s+#',"’", $value);// C’  est
+    $value = preg_replace('#\s+\’#',"’", $value);//C  ’est
+    $value = preg_replace('#\'\s+#',"'", $value);//C'  est
+    $value = preg_replace('#\s+\'#',"'", $value);//C  'est
+    $value = preg_replace('#\(\s+#',"(", $value);//(   je suis belle)
+    $value = preg_replace('#\s+\)#', ")",$value);//(je suis juste extraordinaire   )
+    $value = preg_replace('#\s+,#', ",", $value);// une femme ambitieuse   ,que suis je
+    $value = preg_replace('#,\s+#', ",", $value);//une femme battante,   que suis je
+    $value = preg_replace('#\s+\.#', ".",$value);//il faut toujours etre patient  .
+    $Tableau[]= $value;
+  }
+  foreach ($Tableau as $value)
+  {
+    if (preg_match("#^[a-z]#", $value))
+    {
+        $b=strtoupper($value[0]);
+        $value = preg_replace("#^[a-z]#",$b, $value);
+        $TabCorrige[] =$value; 
+    }
+        else
+        {
+            $TabCorrige[] =$value; 
+        }
+       
+  }
+  foreach ($TabCorrige as  $value) {
+          echo $value;
+        } 
+return  $value;
+}
 
+else{
+  echo "<p>ERROR cette phrase n'as pas de fin!!!</p>";
+}
+
+}
   function premier($value)
   {
     $tab=[];
